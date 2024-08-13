e52a866fb4b91e5324bebb492d889daf428013f1
shubham kumar <shubhamkmuz@gmail.com>
2024-08-14 01:15:37 +0530

    Observer Pattern outline

---
+++ src/ObserverPattern/StatisticsDisplay.java
@@ -0,0 +1,17 @@
+package ObserverPattern;
+
+public class StatisticsDisplay implements DisplayElement, Observer {
+
+	@Override
+	public void update(float temp, float humidity, float pressure) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
